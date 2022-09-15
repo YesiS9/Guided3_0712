e96a6ae7fb435d66b4fdbdf8699db70d41fe58f9
YesiS9 <yesisugandi900@gmail.com>
2022-09-15 08:00:59 +0700

    view_binding_10712

---
+++ app/src/main/java/com/example/view_binding_10712/TaskList.kt
@@ -0,0 +1,16 @@
+package com.example.view_binding_10712
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur",
+            "Kebiasaan sehari-hari untuk membuat hari makin Indah"),
+        Task("Chapter 2", "Nguli Genshin Impact",
+            "Mencari Primo buat Gacha Mama Yaemiko tercinta / Keqing Wangi"),
+        Task("Chapter 3", "Ngopi",
+            "Habis capek Nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4", "dimana mama Ei?",
+            "Buat Adu Mekanik Showcase Ayaka pyro"),
+        Task("Chapter 5", "Upload ke Tiktok",
+            "Buat Pamer ke view Showcase Ayaka Pryro C6 + Bennet C6")
+    )
+}
